76a3e2cf5d9ebbf5a22fdb6ed2dc34080ef5402f
abhinabkanrar <abhinabkanrar@gmail.com>
2017-08-09 16:24:15 +0530

    First commit

---
+++ src/main/java/com/mabsisa/ai/App.java
@@ -0,0 +1,13 @@
+package com.mabsisa.ai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
